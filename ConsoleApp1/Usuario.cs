ce74c10947a7ce5fc684944c558ee93738f1c1e2
Ricardo Mees <ricardobmees@hotmail.com>
2023-10-10 21:26:36 -0300

    Adicionado classe Usuario

---
+++ ConsoleApp1/Usuario.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Usuario
+    {
+        public Usuario() 
+        {
+            Console.WriteLine("Digite o email.");
+            this.Email = Console.ReadLine();
+
+            Console.WriteLine("Digite sua senha");
+            this.Password = Console.ReadLine();
+
+        }
+
+        private string Email;
+        private string PasswordSalt;
+        private string Password
+        {
+            get
+            {
+                return this.Password;
+            }
+            set
+            {
+                this.Password = HashPassword(value);
+            }
+        }
+        private string Nome;
+        private long Telefone;
+        private string Endereco;
+
+        private string HashPassword(string password)
+        {
+            //ToDo: Implementar sistema de hash para a senha.
+            return password;
+        }
+    }
+}
