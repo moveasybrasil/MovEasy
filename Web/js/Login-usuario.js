5fb1db7dcacdac5875035ca3be6d584e11af0d26
Ricardo Mees <ricardobmees@hotmail.com>
2023-12-06 19:04:57 -0300

    Adicionei o manter conectado ao objeto values

--- Web/js/Login-usuario.js
+++ Web/js/Login-usuario.js
@@ -2,10 +2,9 @@ $(() => {
 $("#btn-logar").click(() => {
     const values = {
      email: $("#email-login")[0].value,   
-     senha: $("#senha-login")[0].value
+     senha: $("#senha-login")[0].value,
+     manterConectado: $("#manterConectado")[0].checked
     }
-
-    console.log(values)
 
     if (!values.email) {
         alert("Email não informado!");
@@ -13,14 +12,15 @@ $("#btn-logar").click(() => {
         return;
     }
     $("#email-login").removeClass("invalid");
-
+    
     if (!values.senha) {
         alert("Senha não informada!");
         $("#senha-login").addClass("invalid");
         return;
     }
-
     $("#senha-login").removeClass("invalid");
+    
+    console.log(values)
 });  
   
 })
