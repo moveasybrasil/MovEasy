2713c52fa43b2ba270b423f65bded1ea7f34ab94
VICTOR MATHEUS DA COSTA MARIANO <victor.mariano@alunos.sc.senac.br>
2023-12-06 22:01:08 -0300

    realizado jquery cadastro

--- Web/js/cadastro.js
+++ Web/js/cadastro.js
@@ -1,6 +1,5 @@
 $(() => {
-    $(document).ready(function () {
-
+ 
         $("#button-continuar").click(() => {
             const values = {
                 email: $("#email")[0].value,
@@ -11,56 +10,130 @@ $(() => {
             console.log(values)
 
             let camposInvalidos = false
+            let msg = "Os campos em vermelho estão incorretos!"
 
             if (!values.email) {
                 $('input[id="email"]').css("border", "2px solid red");
                 camposInvalidos = true
                 $("#email").addClass("invalid");
+            } else if (!~values.email.indexOf("@")) {
+                $('input[id="email"]').css("border", "2px solid red");
+                camposInvalidos = true
+                $("#email").addClass("invalid");
+                msg = "Há informações faltando no e-mail informado!"
+            } else if (!~values.email.split("@")[1].indexOf(".")) {
+                $('input[id="email"]').css("border", "2px solid red");
+                camposInvalidos = true
+                $("#email").addClass("invalid");
+                msg = "Há informações faltando no e-mail informado!"
+            } else if (!values.email.split("@")[1].split(".")[1]) {
+                $('input[id="email"]').css("border", "2px solid red");
+                camposInvalidos = true
+                $("#email").addClass("invalid");
+                msg = "Há informações faltando no e-mail informado!"
             } else {
                 $("#email").removeClass("invalid");
                 $('input[id="email"]').css("border", "1px solid #bbb");
             }
 
-            if (!values.senha) {
+            if (values.senha.length<8) {
                 $('input[id="password"]').css("border", "2px solid red");
                 camposInvalidos = true
                 $("#senha").addClass("invalid");
+                $('input[id="confirmed-password"]').css("border", "2px solid red");
+                $("#confirmed-senha").addClass("invalid");
+                msg = "A senha deve ter mais de 8 caracteres!"
+            } else if (!(values.senha == values.confirmar_senha)) {
+                $('input[id="password"]').css("border", "2px solid red");
+                camposInvalidos = true
+                $("#senha").addClass("invalid");
+                $('input[id="confirmed-password"]').css("border", "2px solid red");
+                $("#confirmed-senha").addClass("invalid");
+                msg = "As senhas não são iguais!"
             } else {
                 $("#senha").removeClass("invalid");
                 $('input[id="email"]').css("border", "1px solid #bbb");
             }
 
-            if (!values.confirmar_senha) {
-                $('input[id="confirmed-password"]').css("border", "2px solid red");
-                camposInvalidos = true
-                $("#confirmed-senha").addClass("invalid");
-            } else {
-                $("#confirmed-senha").removeClass("invalid");
-                $('input[id="confirmed-senha"]').css("border", "1px solid #bbb");
-            }
-
-            if(camposInvalidos) {
-                alert("Os campos em vermelho não foram informados!");
+            if (camposInvalidos) {
+                alert(msg);
             } else {
                 $("#box-continuar-cadastro").hide();
                 $("#box-finalizar-cadastro").show();
             }
         });
 
-        // $("#button-continuar").click(() => {
-        //     $("#box-continuar-cadastro").hide();
-        //     $("#box-finalizar-cadastro").show();
-        // });
+
+// kdlfkldfklkdfl
+
+
+        $("#button-cadastrar").click(() => {
+            const values = {
+                nome: $("#nome")[0].value,
+                cpf_or_cnpj: $("#cpf-cnpj")[0].value,
+                telefone: $("#telefone")[0].value
+            }
+
+            console.log(values)
+
+            let camposInvalidosFinal = false
+            let msg = "Os campos em vermelho estão incorretos!"
+
+            if (!values.nome) {
+                $('input[id="nome"]').css("border", "2px solid red");
+                camposInvalidosFinal = true
+                $("#nome").addClass("invalid");
+            } else {
+                $("#nome").removeClass("invalid");
+                $('input[id="nome"]').css("border", "1px solid #bbb");
+            }
+
+            if (!values.cpf_or_cnpj) {
+                $('input[id="cpf-cnpj"]').css("border", "2px solid red");
+                camposInvalidosFinal = true
+                $("#cpf-cnpj").addClass("invalid");
+            } else if (values.cpf_or_cnpj.length<8) {
+                $('input[id="cpf-cnpj"]').css("border", "2px solid red");
+                camposInvalidosFinal = true
+                $("#cpf-cnpj").addClass("invalid");
+                msg = "O seu CPF ou CNPJ deve ter mais de 11 números!"
+            } else if (values.cpf_or_cnpj.length>14) {
+                $('input[id="cpf-cnpj"]').css("border", "2px solid red");
+                camposInvalidosFinal = true
+                $("#cpf-cnpj").addClass("invalid");
+                msg = "O seu CNPJ não deve ter mais de 14 números!"
+            } else {
+                $("#senha").removeClass("invalid");
+                $('input[id="cpf-cnpj"]').css("border", "1px solid #bbb");
+            }
+
+            if (!values.telefone) {
+                $('input[id="telefone"]').css("border", "2px solid red");
+                camposInvalidosFinal = true
+                $("#telefone").addClass("invalid");
+            } else if (!values.cpf_or_cnpj.length==11) {
+                $('input[id="telefone"]').css("border", "2px solid red");
+                camposInvalidosFinal = true  
+                $("#telefone").addClass("invalid");
+                msg = "O seu número de telefone não pode ter mais ou menos de 11 números! Coloque o seu DDD e o número, exemplo: 47999999999."
+            } else {
+                $("#senha").removeClass("invalid");
+                $('input[id="cpf-cnpj"]').css("border", "1px solid #bbb");
+            }
+
+            if (camposInvalidosFinal) {
+                alert(msg);
+            } else {
+                $("#button-cadastrar").click(function () {
+                    msg="Cadastro realizado com sucesso!";
+                });
+            }
+        });
 
         $("#button-voltar").click(() => {
             $("#box-continuar-cadastro").show();
             $("#box-finalizar-cadastro").hide();
         })
-
-        $("#button-cadastrar").click(function () {
-            alert("Cadastro realizado com sucesso!");
-        });
-
     })
-})
+
 // slideToggle(slow) para header mobile
