c5994ad14ac41708a699b43ef8402b2300ca7d2e
Ricardo Mees <ricardobmees@hotmail.com>
2023-12-20 21:27:54 -0300

    Adicionado State

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddTransient<IStateRepository, StateRepository>();
 
 builder.Services.AddCors();
 
