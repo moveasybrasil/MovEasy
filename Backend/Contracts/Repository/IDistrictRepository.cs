171bf6a103f6c97dd9ef7c940b5b54bdc707d2a9
Ricardo Mees <ricardobmees@hotmail.com>
2023-12-21 19:11:59 -0300

    Adicionado District

---
+++ Backend/Contracts/Repository/IDistrictRepository.cs
@@ -0,0 +1,10 @@
+﻿using Backend.DTO;
+using Backend.Entity;
+
+namespace Backend.Contracts.Repository
+{
+    public interface IDistrictRepository
+    {
+        Task<IEnumerable<DistrictEntity>> Get();
+    }
+}
