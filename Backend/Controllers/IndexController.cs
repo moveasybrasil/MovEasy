a5380716c4ea3e4987d3b216acdc02b0d5895809
Ricardo Mees <ricardobmees@hotmail.com>
2024-01-19 10:28:25 -0300

    Adicionei get para o index do servidor

---
+++ Backend/Controllers/IndexController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class IndexController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return Ok("Servidor MovEasy");
+        }
+
+    }
+}
