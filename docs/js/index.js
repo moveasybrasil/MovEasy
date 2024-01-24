75dbaf5c04ceba66e8c190636fc4513744b510d7
EDUARDO BOLSON RAUBER <eduardo.rauber@alunos.sc.senac.br>
2024-01-24 18:46:21 -0300

    index.js alterado

--- docs/js/index.js
+++ docs/js/index.js
@@ -4,35 +4,7 @@ document.querySelectorAll(".header-home").forEach( (e) => {
 })
 
 $(() => {
-
-    $("#buscar-opc").click( ()=> {
-        
-        const values = {
-            origem_estado: $("#origem_estado")[0].value,
-            origem_cidade: $("#origem_cidade")[0].value,
-            destino_estado: $("#destino_estado")[0].value,
-            destino_cidade: $("#destino_cidade")[0].value
-        }
-
-        const texts = {
-            origem_estado: "Estado de Origem",
-            origem_cidade: "Cidade de Origem",
-            destino_estado: "Estado de Destino",
-            destino_cidade: "Cidade de Destino"
-        }
-
-        for(key in values) {
-            if(!values[key]){
-                alert(`${texts[key]} não informado/a!`)
-                $(`#${key}`).addClass("invalid")
-                return
-            } else {
-                $(`#${key}`).removeClass("invalid")
-            }
-        }
-
-        console.log(values);
-
-    })
-
-})
+       $('#text-bt').click( (e) => {
+        goTo('agenda')
+       })
+    });
