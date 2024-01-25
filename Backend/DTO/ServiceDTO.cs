18a0a413e2d88e6c340e854f4910f9d3347e065a
Luiz Eduardo <143532271+luizeduardooo@users.noreply.github.com>
2024-01-25 18:48:30 -0300

    Add files via upload

---
+++ Backend/DTO/ServiceDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.DTO
+{
+    public class ServiceDTO
+    {
+        public string Description { get; set; }
+        public string OriginDescription { get; set; }
+        public int Status { get; set; }
+        public string DestinationDescription { get; set; }
+        public string Date { get; set; }
+        public string Obs { get; set; }
+    }
+}
