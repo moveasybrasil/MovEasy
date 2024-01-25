1fdb8cd9c5def53691fe8305036bba8809e0df4f
Luiz Eduardo <143532271+luizeduardooo@users.noreply.github.com>
2024-01-25 18:46:18 -0300

    Add files via upload

---
+++ Backend/Controllers/ServiceController.cs
@@ -0,0 +1,58 @@
+﻿using Backend.Contracts.Repository;
+using Backend.DTO;
+using Backend.Entity;
+using Backend.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("service")]
+    public class ServiceController : ControllerBase
+    {
+        private readonly IServiceRepository _serviceRepository;
+
+        public ServiceController(IServiceRepository serviceRepository)
+        {
+            _serviceRepository = serviceRepository;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ServiceDTO service)
+        {
+            await _serviceRepository.Add(service);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _serviceRepository.Delete(id);
+            return Ok();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _serviceRepository.Get());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await _serviceRepository.GetById(id));
+        }
+
+        [HttpPut]
+        [Authorize]
+        public async Task<IActionResult> Update(ServiceEntity service)
+        {
+            await _serviceRepository.Update(service);
+            return Ok();
+        }
+    }
+}
