26ab0ab8a7e8985e63df22302d85e37d5f5a5f14
Ricardo Mees <ricardobmees@hotmail.com>
2024-01-26 19:05:24 -0300

    Corrigi problema de salt mudar todo reinicio de server

--- Backend/Infrastructure/PasswordHasher.cs
+++ Backend/Infrastructure/PasswordHasher.cs
@@ -8,13 +8,13 @@ namespace Backend.Infrastructure
     public class PasswordHasher : Connection
     {
 
-        public string SaltPath = "salt.txt";
+        private static string Salt = "J5c8\\u002BtzsYCEb\\u002BWcXHrDsYA==";
 
         public async Task<string> HashPassword(string Password)
         {
             using (SHA256 sha256 = SHA256.Create())
             {
-                string salt = await GetSalt();
+                string salt = PasswordHasher.Salt;
                 byte[] passwordBytes = Encoding.UTF8.GetBytes(Password + salt);
                 byte[] hashBytes = sha256.ComputeHash(passwordBytes);
                 string hashString = Convert.ToBase64String(hashBytes);
@@ -49,45 +49,5 @@ namespace Backend.Infrastructure
                 return salt;
             }
         }
-
-        private async Task<string> GetSalt()
-        {
-            string salt = string.Empty;
-            try
-            {
-                string json = File.ReadAllText(this.SaltPath);
-                salt = JsonSerializer.Deserialize<String>(json);
-            }
-            catch (System.IO.FileNotFoundException)
-            {
-                salt = await SetSalt();
-            }
-            catch (Exception ex)
-            {
-                Console.Clear();
-                Console.WriteLine($"\nErro ao ler arquivo de dados.\n*Err: {ex.Message}");
-            }
-
-            return salt;
-        }
-
-        private async Task<string> SetSalt()
-        {
-            string data = Convert.ToBase64String(GenerateSalt());
-
-            try
-            {
-                string json = JsonSerializer.Serialize(data);
-                File.WriteAllText(this.SaltPath, json);
-            }
-            catch (Exception ex)
-            {
-                Console.Clear();
-                Console.WriteLine($"\nErro ao salvar arquivo de dados.\n*Err: {ex.Message}");
-            }
-
-            return data;
-        }
-
     }
 }
