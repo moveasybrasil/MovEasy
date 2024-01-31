3e89ca8cebda8844573a66803ba1942ab8f65dd2
Eduardo Rauber <eduardo_farm@hotmail.com>
2024-01-31 13:30:34 -0300

    componentes funcionais pagamento

--- docs/js/qr-code.js
+++ docs/js/qr-code.js
@@ -1,3 +1,8 @@
+    
+    /*------------------------------------------------
+    Função que tira o blur e começa o countdown do pix
+    -------------------------------------------------*/
+
 function GerarQRCode() {
     let finalURL = './assets/images/Qr-Before.png';
     if ($('#copia-cola').text().trim() !== '') {
@@ -6,7 +11,8 @@ function GerarQRCode() {
     $('.blur').toggleClass('blur');
     $('#img-qr').attr('src', finalURL);
     $('#copia-cola').append("https://moveasybrasil.github.io/MovEasy")
-
+    $('.hidden').toggleClass('hidden')
+    startTimer(600, document.querySelector('#timer'));
 }
 
 $(document).ready(function () {
@@ -15,9 +21,47 @@ $(document).ready(function () {
     });
 });
 
-$(document).ready(function() {
-    $("#btn-copiar").click(function() {
-      var conteudoTextarea = $("#copia-cola").val();
-      navigator.clipboard.writeText(conteudoTextarea);
+    /*-----------------
+    Timer de pagamento
+    -----------------*/
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+/*------------------------------------
+Função para copiar o texto do textarea
+-------------------------------------*/
+
+$(document).ready(function () {
+    $("#btn-copiar").click(function () {
+        var conteudoTextarea = $("#copia-cola").val();
+        navigator.clipboard.writeText(conteudoTextarea);
     });
+});
+
+/*-------------------------
+Alternar entre Pix e Crédito
+-------------------------*/
+$(document).ready(function () {
+$('.toggle-checkbox').change(function () {
+    if (this.checked) {
+      $('.checkout-box').addClass('blur');
+    } else {
+      $('.checkout-box').removeClass('blur');
+    }
   });
+});
