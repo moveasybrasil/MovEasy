8bd7ae0f7957969687b76d972e94ab75268ab44e
Ricardo Mees <ricardobmees@hotmail.com>
2024-02-01 21:22:01 -0300

    Backend e javascript do suporte

---
+++ Backend/Controllers/SupportController.cs
@@ -0,0 +1,28 @@
+﻿using Backend.DTO;
+using Backend.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("support")]
+    public class SupportController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Get(SupportDTO support)
+        {
+            try
+            {
+                Email email = new Email();
+                return Ok(await email.SendEmail(
+                    "noreply.moveasy@gmail.com", 
+                    support.Subject, 
+                    $"Email de {support.Name} [{support.Email}]\n\n{support.Body}"
+                ));
+            } catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Backend/DTO/SupportDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend.DTO
+{
+    public class SupportDTO
+    {
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
