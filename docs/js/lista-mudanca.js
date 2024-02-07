758011beb6ff7f559974a1927506edac752c7499
Eduardo Rauber <eduardo_farm@hotmail.com>
2024-02-07 14:19:06 -0300

    lista

--- docs/js/lista-mudanca.js
+++ docs/js/lista-mudanca.js
@@ -1,26 +1,30 @@
-function createItemList(item) {
-    const newItem = $('#info-mudanca').clone().removeAttr('id').removeClass('hidden');
-    $('data', newItem).html(item.data);
-    $('usuario', newItem).html(item.usuario);
-    $('origem', newItem).html(item.origem);
-    $('destino', newItem).html(item.destino);
-    $('descricao', newItem).html(item.descricao);
-    $('valor', newItem).html(item.valor);
+$(() => {
 
-    $('#dados-lista').append($(item));
+function criarListaMudanca(item) {
+    const novoItem = $("#modelo-mudanca").clone().removeAttr(`id`).removeClass('hidden');
+    $('.data', novoItem).html(item.data);
+    $('.usuario', novoItem).html(item.usuario);
+    $('.origem', novoItem).html(item.origem);
+    $('.destino', novoItem).html(item.destino);
+    $('.descricao', novoItem).html(item.descricao);
+    $('.valor', novoItem).html(item.valor);
+
+    $('#dados-lista').append($(novoItem));
 }
 
 const listaMudanca = []
 
 listaMudanca.push({
-    data: "24/02/2024 - 08:15h às 10:12h",
+    data: "24/02/2024<br/> 08:15h às 10:12h",
     usuario: "Eduardo",
-    origem: "Origem: Rua Pindamonhangaba, 3132 - Bluemanu, SC",
+    origem: "Rua Pindamonhangaba, 3132 - Bluemanu, SC",
     destino: "Rua Carlota, 2012 - Bluemanu, SC",
     descricao: "Mudança completa, apartamento no 3º andar",
-    valor: "R$ 899,87"
+    valor: "899,87"
 })
 
 for (let item of listaMudanca) {
-    createItemList(item);
+    criarListaMudanca(item);
 }
+
+});
