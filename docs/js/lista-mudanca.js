e49e0a073e10ee4039479487cd785524f098b577
EDUARDO BOLSON RAUBER <eduardo.rauber@alunos.sc.senac.br>
2024-02-08 21:54:21 -0300

    adequação desktop lista mudança

--- docs/js/lista-mudanca.js
+++ docs/js/lista-mudanca.js
@@ -2,6 +2,7 @@ $(() => {
 
 function criarListaMudanca(item) {
     const novoItem = $("#modelo-mudanca").clone().removeAttr(`id`).removeClass('hidden');
+    $('.img-perfil', novoItem).html(item.data);
     $('.data', novoItem).html(item.data);
     $('.usuario', novoItem).html(item.usuario);
     $('.origem', novoItem).html(item.origem);
@@ -16,27 +17,28 @@ const listaMudanca = []
 
 listaMudanca.push({
     data: "24/02/2024<br/> 08:15h às 10:12h",   
-    usuario: "Eduardo",
-    origem: "Rua Pindamonhangaba, 3132 <br/> Bluemanu, SC",
-    destino: "Rua Carlota, 2012 <br/> Blumenau, SC",
+    usuario: "Eduardo ★ 4.99",
+    origem: "Rua Pindamonhangaba, 3132 - Blumenau, SC",
+    destino: "Rua Carlota, 2012 - Blumenau, SC",
     descricao: "Mudança completa, apartamento no 3º andar",
     valor: "899,87"
 })
 
 listaMudanca.push({
     data: "25/02/2024<br/> 14:27h às 16:12h",
-    usuario: "Victor",
-    origem: "Rua São Paulo, 276 <br/> Blumenau, SC",
-    destino: "Rua Iguaçu, 1987 <br/> Blumenau, SC",
+    usuario: "Victor ★ 5.00",
+    origem: "Rua São Paulo, 276 - Blumenau, SC",
+    
+    destino: "Rua Iguaçu, 1987 - Blumenau, SC",
     descricao: "Geladeira e Freezer apenas, casa",
     valor: "152,04"
 });
 
 listaMudanca.push({
     data: "29/02/2024<br/> 10:24h às 11:16h",
-    usuario: "Luiz",
-    origem: "Rua Francisco Sênior, 987 <br/> Blumenau, SC",
-    destino: "Rua Jerõnimo, 577  <br/> Gaspar, SC",
+    usuario: "Luiz ★ 4.98",
+    origem: "Rua Francisco Sênior, 987 - Blumenau, SC",
+    destino: "Rua Jerõnimo, 577 - Gaspar, SC",
     descricao: "Sofá, apartamento 8º andar",
     valor: "454,12"
 })
@@ -49,11 +51,17 @@ for (let item of listaMudanca) {
 
 $(() => {
 
-$('.btn-aceite').click(function() {
-    // console.log($('.usuario, .descricao', $(this).closest('.card')));
-    // console.log($('.usuario, .descricao')); <- mostra no console qual é o contexto
+    $(document).on('click', '#btn-visualizar', function() {
+    //console.log($('.usuario, .descricao', $(this).closest('.card')));
+    //console.log($('.usuario, .descricao')); 
     $('.usuario, .descricao', $(this).closest('.card')).removeClass('hidden').hide().slideDown();
-    $(this).text('Aceitar');
+    $(this).hide();
+    console.log($('#btn-aceite'));
+    $('.btn-aceite', $(this.closest('.card'))).show();
+});
+
+    $(document).on('click', '.btn-aceite', $this.closest('.card'), function() {
+        //lógica quando aceitar uma mudança.
 });
 
 });
