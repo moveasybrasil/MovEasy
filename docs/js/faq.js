1f7d9d5ff9205eb098b2465a430b7d34925c686b
Victtoor <victormatheus.mariano7@gmail.com>
2024-02-20 19:53:19 -0300

    faq: ortografia arrumada

--- docs/js/faq.js
+++ docs/js/faq.js
@@ -22,22 +22,22 @@ lista.push({
 })
 lista.push({
     pergunta: "Quais são os métodos de pagamento aceitos?",
-    resposta: "Aceitamos cartão de crédito, PIX, e boleto."
+    resposta: "Aceitamos cartão de crédito, débito, PIX, e boleto."
 })
 lista.push({
     pergunta: "É oferecido serviço de desmontagem e montagem de móveis?",
-    resposta: "Isto deve ser tratado com o motorista."
+    resposta: "Isto deve ser tratado a parte com o motorista."
 })
 lista.push({
     pergunta: "Como posso obter um orçamento para a minha mudança?",
-    resposta: "Você pode acessar a página \"Agendar\" para receber uma estimativa. Tenha em mente que os preços podem sofrer alterações até a finalização do pedido."
+    resposta: "Você pode acessar a página \"Mudanças\" para receber uma estimativa. Tenha em mente que os preços podem sofrer alterações até a finalização do pedido."
 })
 lista.push({
-    pergunta: "Posso agendar uma mudança em um fim de semana ou feriado?",
-    resposta: "Sim, você pode agendar uma mudança para qualquer data, mas não podemos garantir que haverá alguem para lhe atender."
+    pergunta: "Posso agendar uma mudança em um final de semana ou feriado?",
+    resposta: "Sim, você pode agendar uma mudança para qualquer data, mas não podemos garantir que haverá alguém para lhe atender."
 })
 lista.push({
-    pergunta: "O que devo fazer ser precisar reagendar minha mudança?",
+    pergunta: "O que devo fazer se precisar reagendar minha mudança?",
     resposta: "Se o reagendamento for antes do tempo limite, você pode cancelar o pedido e realizar um novo agendamento. Se estiver dentro do tempo limite, uma multa será aplicada ao pedido."
 })
 
