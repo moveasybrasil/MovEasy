46b8eae96a8a0a50cb2b2209561c03f2fc195274
victtoor <victormatheus.mariano7@gmail.com>
2024-02-28 18:51:45 -0300

    orçamento: header arrumado

--- docs/js/orcamento.js
+++ docs/js/orcamento.js
@@ -1,21 +1,23 @@
+// Adiciona a classe 'active' ao header para o item específico
+document.querySelector(".header-moving").classList.add("active");
 
-    function submitForm() {
-        var name = document.querySelector('[data-placeholder="Digite seu nome"]').innerText;
-        var email = document.querySelector('[data-placeholder="Digite seu email"]').innerText;
-        var message = document.querySelector('[data-placeholder="Digite sua mensagem"]').innerText;
-        
-        // Aqui você pode fazer o que desejar com os dados, como enviar para um servidor, por exemplo.
-        console.log("Nome: " + name);
-        console.log("Email: " + email);
-        console.log("Mensagem: " + message);
-        
-        // Limpa os campos após o envio
-        document.querySelectorAll('.editable').forEach(function(element) {
-            element.innerText = '';
-        });
-        
-        alert("Orçamento enviado!");
-    }
+function submitForm() {
+    var name = document.querySelector('[data-placeholder="Digite seu nome"]').innerText;
+    var email = document.querySelector('[data-placeholder="Digite seu email"]').innerText;
+    var message = document.querySelector('[data-placeholder="Digite sua mensagem"]').innerText;
+
+    // Aqui você pode fazer o que desejar com os dados, como enviar para um servidor, por exemplo.
+    console.log("Nome: " + name);
+    console.log("Email: " + email);
+    console.log("Mensagem: " + message);
+
+    // Limpa os campos após o envio
+    document.querySelectorAll('.editable').forEach(function (element) {
+        element.innerText = '';
+    });
+
+    alert("Orçamento enviado!");
+}
 
 var valorTotal;
 
@@ -40,7 +42,7 @@ function exibirDadosNaPaginaOrcamento() {
 
         const valorDiv = document.getElementById('orcamentoTotal');
         valorDiv.innerHTML = `<h2>R$ ${valorTotal},00</h2>`;
-        
+
     } else {
         console.error('Dados de origem e/ou destino não encontrados no localStorage.');
     }
